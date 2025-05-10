5c8ac9b113e7c87d2d181ea591f60fb2d6e0d0d2
Ewertong1 <111665177+Ewertong1@users.noreply.github.com>
2025-05-10 15:55:05 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,5 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
+
   apiUrl: 'https://santaizabel.net.br/api'
-=======
-  apiUrl: 'https://santaizabel.net.br/api'
->>>>>>> 490de9f181ebce6e760d504be9ff6f2bbbc7536a
 };
